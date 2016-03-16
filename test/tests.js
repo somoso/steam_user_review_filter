cb9f9562b1f3b6970a08568efe7f20e8e753e01b
Jesse Bordoe <jesse.bordoe@gmail.com>
2016-03-16 15:41:04 +0000

    Add some test cases

--- test/tests.js
+++ test/tests.js
@@ -2,10 +2,30 @@ QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
-QUnit.test( "basic_match", function( assert ) {
+QUnit.test( "rating_match_1", function( assert ) {
+  assert.ok( filter_steam("10/10 would play again") == true, "Passed!" );
+});
+
+QUnit.test( "ign_match_1", function( assert ) {
   assert.ok( filter_steam("ign") == true, "Passed!" );
 });
 
-QUnit.test( "slightly_complex_match", function( assert ) {
+QUnit.test( "ign_match_2", function( assert ) {
+  assert.ok( filter_steam("I love IGN!") == true, "Passed!" );
+});
+
+QUnit.test( "ign_no_match_1", function( assert ) {
   assert.ok( filter_steam("design") == false, "Passed!" );
+});
+
+QUnit.test( "best_ever_match_1", function( assert ) {
+  assert.ok( filter_steam("Best game ever") == true, "Passed!" );
+});
+
+QUnit.test( "best_ever_match_2", function( assert ) {
+  assert.ok( filter_steam("Best Macklemore game ever made") == true, "Passed!" );
+});
+
+QUnit.test( "best_ever_no_match_1", function( assert ) {
+  assert.ok( filter_steam("Best Macklemore game. Not ever") == false, "Passed!" );
 });
