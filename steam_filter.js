4dbb637d635021de8704060e4cad311da4e25ce2
Jesse Bordoe <jesse.bordoe@gmail.com>
2016-03-16 15:38:45 +0000

    Polyfill 'includes' method
    Apparently doesn't yet work in some javascript implementations (didn't work for me)
    See 'Polyfill' here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes

--- steam_filter.js
+++ steam_filter.js
@@ -1,3 +1,18 @@
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
+
 function filter_steam(content) {
     if (content.includes('10/10')) {
         return true;
